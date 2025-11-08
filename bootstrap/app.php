95bd7fdc5dadc64857d09de31b58e4ebc6d4df41
Hiikey7 <kelvinramsiel@gmail.com>
2025-11-09 00:10:24 +0300

    fix(middleware): register signed middleware alias for campaign approval routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias('signed', \Illuminate\Routing\Middleware\ValidateSignature::class);
+        
         $middleware->append(function ($request, $next) {
             $response = $next($request);
             
