6c68eca457ce5aff109725a47c09cbf923c68ff6
Hiikey7 <kelvinramsiel@gmail.com>
2025-11-09 00:22:38 +0300

    fix(middleware): pass array to use() method for global middleware registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         ]);
         
-        $middleware->use(\App\Http\Middleware\CorsMiddleware::class);
+        $middleware->use([\App\Http\Middleware\CorsMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
